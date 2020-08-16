17e0424ca37c28afa178930fc4c0d87e8cb1ca5f
DrJfrost <julianrojas99@hotmail.com>
2020-08-15 17:47:57 -0700

    sreializers and models for reservations

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from users.models import User
+from movies.models import Spot, Show
 
+class BillType(models.Model):
+    name = models.CharField(max_length=25)
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=25)
+
+class ResState(models.Model):
+
+    name = models.CharField(max_length=30)
+
+class Reservation(models.Model):
+    
+    creation = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    scoring = models.PositiveIntegerField(default=0)
+
+    #foreign keys
+    state = models.ForeignKey(ResState, on_delete=models.PROTECT, verbose_name='state of the reservation')
+    spots = models.ManyToManyField(Spot, on_delete=models.PROTECT, verbose_name='spots that were booked')
+    show = models.ForeignKey(Show, on_delete=models.PROTECT, verbose_name='movie that was booked')
+
+
+class Bill(models.Model):
+    
+    value = models.FloatField()
+    saved_score = models.FloatField()
+    
+    #foreign keys
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='client owner of the bill')
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT, verbose_name='payment method of the bill')
+    reservation = models.ForeignKey(Reservation, on_delete=models.PROTECT, verbose_name='reservation of the bill', related_name='reservation')
 # Create your models here.
---
+++ reservations/serializers.py
@@ -0,0 +1,79 @@
+from reservations.models import BillType, PaymentMethod, ResState, Reservation, Bill
+from rest_framework import serializers
+
+class BillTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BillType
+        fields = ['id', 'name']
+
+
+class PaymentMethodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PaymentMethod
+        fields = ['id', 'name']
+
+
+class ResStateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ResState
+        fields = ['id', 'name']
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reservation
+        fields = ['id', 'description', 'scoring', 'state', 'spots', 'show']
+
+
+class BillSerializer(serializers.ModelSerializer):
+
+    reservation = ReservationSerializer()
+
+    class Meta:
+        model = Bill
+        fields = ['id', 'value', 'user', 'bill_type', 'payment_method', 'reservation']
+
+    def create(self, validated_data):
+
+        #reservation info
+        reservation_data = validated_data.pop('reservation')
+        spots = reservation_data.pop('spots')
+
+        bill = Bill.objects.create(**validated_data)
+
+        reservation = Reservation.objects.create(**reservation_data)
+        reservation.spots.add(**spots)
+        reservation.save()
+
+
+        bill.save()
+        
+        return bill
+
+    def update(self, instance, validated_data):
+
+        #reservation info
+        reservation_data = validated_data.pop('reservation')
+        reservation = instance.reservation
+
+        instance.value = validated_data.get('value', instance.value)
+        instance.saved_score = validated_data.get('saved_score', instance.saved_score)
+        instance.bill_type = validated_data.get('bill_tytpe', instance.bill_type)
+        instance.payment_method = validated_data.get('payment_method', instance.payment_method)
+
+        if 'reservation' in validated_data:
+            reservation.description = reservation_data.get('description', reservation.description)
+            reservation.scoring = reservation_data.get('scoring', reservation.scoring)
+            reservation.state = reservation_data.get('state', reservation.state)
+            if 'spots' in reservation_data:
+                reservation.spots.clear()
+                reservation.spots.add(**reservation_data.get('spots'))
+            reservation.save()
+        
+        instance.save()
+
+        return instance
